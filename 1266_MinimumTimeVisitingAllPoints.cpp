602898c0ad2635f4b5bab0f88dd6498279accf17
Anku <ankitkumarmuhal3@gmail.com>
2025-09-12 23:25:08 +0530

    1266

---
+++ 1266_MinimumTimeVisitingAllPoints.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include <cstdlib>
+using namespace std;
+
+class Solution {
+public:
+  int minTimeToVisitAllPoints(vector<vector<int>> &points) {
+    int time = 0;
+    for (int i = 0; i < points.size() - 1; i++) {
+      int x = abs(points[i + 1][0] - points[i][0]);
+      int y = abs(points[i + 1][1] - points[i][1]);
+      time += max(x, y);
+    }
+    return time;
+  }
+};
