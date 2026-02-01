7e4c3d79dc28437429c4d667e6cd3b5982b6b175
Anku <ankitkumarmuhal3@gmail.com>
2026-02-01 22:33:05 +0530

    977

---
+++ 977_SquaresOfSortedArray.cpp
@@ -0,0 +1,15 @@
+#include <algorithm>
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+  vector<int> sortedSquares(vector<int> &nums) {
+    for (int i = 0; i < nums.size(); i++) {
+      nums[i] = nums[i] * nums[i];
+    }
+    sort(nums.begin(), nums.end());
+    return nums;
+  }
+};
