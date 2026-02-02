32cafe82b49e7e89177a99e7d90e621b5ac27459
Anku <ankitkumarmuhal3@gmail.com>
2026-02-02 09:02:08 +0530

    977 optimized

--- 977_SquaresOfSortedArray.cpp
+++ 977_SquaresOfSortedArray.cpp
@@ -3,7 +3,28 @@
 #include <vector>
 using namespace std;
 
-class Solution {
+class Solution { // O(n)
+public:
+  vector<int> sortedSquares(vector<int> &nums) {
+    vector<int> ans;
+    int lp = 0;
+    int rp = nums.size() - 1;
+
+    while (lp <= rp) {
+      if (nums[lp] * nums[lp] > nums[rp] * nums[rp]) {
+        ans.push_back(nums[lp] * nums[lp]);
+        lp++;
+      } else {
+        ans.push_back(nums[rp] * nums[rp]);
+        rp--;
+      }
+    }
+    reverse(ans.begin(), ans.end());
+    return ans;
+  }
+};
+
+class MySolution {
 public:
   vector<int> sortedSquares(vector<int> &nums) {
     for (int i = 0; i < nums.size(); i++) {
