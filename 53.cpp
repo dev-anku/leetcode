73fd5b810c0acf54b17b9caad750268ca2f282c4
Anku <ankitkumarmuhal3@gmail.com>
2026-05-09 02:55:35 +0530

    53

---
+++ 53.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+  int maxSubArray(vector<int> &nums) {
+    int maxSum = nums[0];
+    int currSum = 0;
+
+    for (int n : nums) {
+      if (currSum < 0) {
+        currSum = 0;
+      }
+      currSum += n;
+      maxSum = max(maxSum, currSum);
+    }
+
+    return maxSum;
+  }
+};
