6e08d0935e5ff6299b1300a199d4263429380d45
Anku <ankitkumarmuhal3@gmail.com>
2026-05-12 16:41:54 +0530

    1665

---
+++ 1665.cpp
@@ -0,0 +1,30 @@
+#include <algorithm>
+#include <bits/stdc++.h>
+#include <climits>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+  static bool cmp(vector<int> &a, vector<int> &b) {
+    return ((a[1] - a[0]) > (b[1] - b[0]));
+  }
+
+  int minimumEffort(vector<vector<int>> &tasks) {
+    sort(tasks.begin(), tasks.end(), cmp);
+
+    int result = 0;
+    int curr = 0;
+
+    for (int i = 0; i < tasks.size(); i++) {
+      if (curr < tasks[i][1]) {
+        int diff = tasks[i][1] - curr;
+        result += diff;
+        curr += diff;
+      }
+      curr = curr - tasks[i][0];
+    }
+
+    return result;
+  }
+};
