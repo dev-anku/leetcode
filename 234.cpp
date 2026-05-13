64e0a3fc4b18cbe824860acb412d7757b564ea2b
Anku <ankitkumarmuhal3@gmail.com>
2026-05-14 00:56:33 +0530

    234

---
+++ 234.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+  bool isPalindrome(ListNode *head) {
+    ListNode *prev = NULL;
+    ListNode *slow = head;
+    ListNode *fast = head;
+
+    while (fast != NULL && fast->next != NULL) {
+      fast = fast->next->next;
+
+      ListNode *next = slow->next;
+      slow->next = prev;
+      prev = slow;
+      slow = next;
+    }
+
+    if (fast != NULL) {
+      slow = slow->next;
+    }
+
+    while (prev != NULL && slow != NULL) {
+      if (prev->val != slow->val) {
+        return false;
+      }
+      prev = prev->next;
+      slow = slow->next;
+    }
+
+    return true;
+  }
+};
