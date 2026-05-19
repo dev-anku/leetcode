fe010dc088553baa957c7b818266f0c0ce23a706
Anku <ankitkumarmuhal3@gmail.com>
2026-05-19 18:30:51 +0530

    232

---
+++ 232.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#include <stack>
+using namespace std;
+
+class MyQueue {
+private:
+  stack<int> in, out;
+
+  void transfer() {
+    while (!in.empty()) {
+      out.push(in.top());
+      in.pop();
+    }
+  }
+
+public:
+  MyQueue() {}
+
+  void push(int x) { in.push(x); }
+
+  int pop() {
+    if (out.empty()) {
+      transfer();
+    }
+
+    int x = out.top();
+    out.pop();
+    return x;
+  }
+
+  int peek() {
+    if (out.empty()) {
+      transfer();
+    }
+
+    return out.top();
+  }
+
+  bool empty() { return in.empty() && out.empty(); }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
