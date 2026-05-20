d13db3a13ea4f2b26bf23d64bd66c8cb5b4067c0
Anku <ankitkumarmuhal3@gmail.com>
2026-05-21 02:42:12 +0530

    2657 opt

--- 2657.cpp
+++ 2657.cpp
@@ -5,6 +5,31 @@ using namespace std;
 class Solution {
 public:
   vector<int> findThePrefixCommonArray(vector<int> &A, vector<int> &B) {
+    int n = A.size();
+
+    vector<int> output;
+    vector<int> freq(n + 1, 0);
+
+    int common = 0;
+
+    for (int i = 0; i < n; i++) {
+      freq[A[i]]++;
+      if (freq[A[i]] == 2)
+        common++;
+      freq[B[i]]++;
+      if (freq[B[i]] == 2)
+        common++;
+
+      output.push_back(common);
+    }
+
+    return output;
+  }
+};
+
+class MySolution {
+public:
+  vector<int> findThePrefixCommonArray(vector<int> &A, vector<int> &B) {
     vector<int> output;
     int n = A.size();
     vector<int> freq(n, 0);
