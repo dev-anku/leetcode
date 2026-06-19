b2c5421786922195f0133032f656f246aca4e0a7
Anku <ankitkumarmuhal3@gmail.com>
2026-06-19 17:11:25 +0530

    maximum depth of binary tree

---
+++ 104.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+  int maxDepth(TreeNode *root) {
+    if (!root)
+      return 0;
+
+    queue<TreeNode *> que;
+    que.push(root);
+    int depth = 0;
+
+    while (!que.empty()) {
+      int n = que.size();
+
+      for (int i = 0; i < n; i++) {
+        TreeNode *node = que.front();
+        que.pop();
+        if (node->left)
+          que.push(node->left);
+        if (node->right)
+          que.push(node->right);
+      }
+
+      depth++;
+    }
+
+    return depth;
+  }
+};
