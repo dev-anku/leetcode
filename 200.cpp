cf340dfcce1d4ed39e6b588947b4605febea2640
Anku <ankitkumarmuhal3@gmail.com>
2026-09-17 19:14:45 +0530

    200 | Number of islands

--- 200.cpp
+++ 200.cpp
@@ -5,6 +5,38 @@ using namespace std;
 
 class Solution {
 public:
+  void dfs(vector<vector<char>> &grid, int i, int j) {
+    if (i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() ||
+        grid[i][j] != '1')
+      return;
+    grid[i][j] = '0';
+    dfs(grid, i, j + 1);
+    dfs(grid, i + 1, j);
+    dfs(grid, i, j - 1);
+    dfs(grid, i - 1, j);
+  };
+
+  int numIslands(vector<vector<char>> &grid) {
+    int m = grid.size();
+    int n = grid[0].size();
+
+    int result = 0;
+
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        if (grid[i][j] == '1') {
+          result++;
+          dfs(grid, i, j);
+        }
+      }
+    }
+
+    return result;
+  }
+};
+
+class FudduSolution {
+public:
   void exploringIsland(vector<vector<char>> &grid, int r, int c) {
     queue<pair<int, int>> q;
 
